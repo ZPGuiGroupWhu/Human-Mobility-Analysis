f1addb7b69685cf3d6d5fbab61e0825d9d2ef9e4
jtwang7 <957113460@qq.com>
2021-06-06 12:22:13 +0800

    添加数据请求错误处理逻辑

--- WebVisualizationSystem/src/common/hooks/useReqData.js
+++ WebVisualizationSystem/src/common/hooks/useReqData.js
@@ -4,17 +4,22 @@ import { useState, useEffect } from 'react';
  * data request
  * @param {function} reqMethod - 请求数据方法
  */
-export const useReqData = (reqMethod, params=null) => {
+export const useReqData = (reqMethod, params = null) => {
   const [data, setData] = useState([]);
   const [isComplete, setComplete] = useState(false);
   // 请求数据
   useEffect(() => {
     async function getData() {
       console.time();
-      // 数据请求
-      let data = await reqMethod(params);
-      setData(data);
-      setComplete(true);
+      try {
+        // 数据请求
+        let data = await reqMethod(params);
+        // 若请求失败，数据为 undefined 时设置默认值
+        setData(data || []);
+        setComplete(true);
+      } catch (err) {
+        console.log(err);
+      }
       console.timeEnd();
     }
     getData();

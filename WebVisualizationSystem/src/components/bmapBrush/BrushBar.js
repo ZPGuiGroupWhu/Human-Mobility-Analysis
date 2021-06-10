ebfd02044cd880074e546be92e5fb9ad548a8140
jtwang7 <957113460@qq.com>
2021-06-10 22:03:22 +0800

    优化 BrushBar，可从多方向刷选数据

--- WebVisualizationSystem/src/components/bmapBrush/BrushBar.js
+++ WebVisualizationSystem/src/components/bmapBrush/BrushBar.js
@@ -26,6 +26,42 @@ export default function BrushBar(props) {
   const [overLayers, setOverLayers] = useState([]); // 覆盖物数组
   const [selected, setSelected] = useState({}); // 选择数组
 
+  /**
+   * 通过起点与终点经纬度关系判断筛选框绘制的方向
+   * @param {number[][]} rectPts - 筛选框的四个角，顺序从起点开始顺时针排布
+   */
+  function paintDirection(rectPts) {
+    switch (true) {
+      case (((rectPts[0].lng - rectPts[2].lng) < 0) && ((rectPts[0].lat - rectPts[2].lat) > 0)):
+        return 'lefttop2rightbottom';
+      case (((rectPts[0].lng - rectPts[2].lng) < 0) && ((rectPts[0].lat - rectPts[2].lat) < 0)):
+        return 'leftbottom2righttop';
+      case (((rectPts[0].lng - rectPts[2].lng) > 0) && ((rectPts[0].lat - rectPts[2].lat) > 0)):
+        return 'righttop2leftbottom';
+      case (((rectPts[0].lng - rectPts[2].lng) > 0) && ((rectPts[0].lat - rectPts[2].lat) < 0)):
+        return 'rightbottom2lefttop';
+    }
+  }
+
+  /**
+   * 根据类型，返回对应的左下角与右上角坐标
+   * @param {number[][]} rectPts - 筛选框的四个角，顺序从起点开始顺时针排布
+   * @param {string} type - 类型
+   * @returns {number[][]} - [左下角坐标，右上角坐标]
+   */
+  function getLBandRT(rectPts, type) {
+    switch (type) {
+      case 'lefttop2rightbottom':
+        return [rectPts[3], rectPts[1]];
+      case 'leftbottom2righttop':
+        return [rectPts[0], rectPts[2]];
+      case 'righttop2leftbottom':
+        return [rectPts[2], rectPts[0]];
+      case 'rightbottom2lefttop':
+        return [rectPts[1], rectPts[3]];
+    }
+  }
+
 
 
   useEffect(() => {
@@ -57,18 +93,24 @@ export default function BrushBar(props) {
       setOverLayers(prev => ([...prev, e.overlay]));
       // 获取选框内点信息
       // See https://www.cnblogs.com/zdd2017/p/13495908.html
+      // getPath 返回的边框角: 从起点开始顺时针排布
       const pts = e.overlay.getPath();
-      const leftTop = pts[3];
-      const rightBottom = pts[1];
-      const ptLeftTop = new BMap.Point(leftTop.lng, leftTop.lat);
-      const ptRightBottom = new BMap.Point(rightBottom.lng, rightBottom.lat);
-      const bound = new BMap.Bounds(ptLeftTop, ptRightBottom);
+      // console.log(pts);
+      // 判断筛选框绘制方向
+      const directionType = paintDirection(pts);
+      // 获取左下角与右上角
+      const [leftBottom, rightTop] = getLBandRT(pts, directionType);
+      const ptLeftBottom = new BMap.Point(leftBottom.lng, leftBottom.lat);
+      const ptRightTop = new BMap.Point(rightTop.lng, rightTop.lat);
+      // 创建 bound 需要边界的西南角和东北角
+      // See https://mapopen-pub-jsapi.bj.bcebos.com/jsapi/reference/jsapi_reference.html#a1b2
+      const bound = new BMap.Bounds(ptLeftBottom, ptRightTop);
       // 临时存储对象
       const obj = {}
       for (let [key, value] of Object.entries(data)) {
         // 设置默认值 []
         Reflect.set(obj, key, []);
-        for (let {id, coord: [lng, lat]} of Object.values(value)) {
+        for (let { id, coord: [lng, lat] } of Object.values(value)) {
           let pt = new BMap.Point(lng, lat);
           if (BMapLib.GeoUtils.isPointInRect(pt, bound)) {
             // 存储对应索引，方便查找

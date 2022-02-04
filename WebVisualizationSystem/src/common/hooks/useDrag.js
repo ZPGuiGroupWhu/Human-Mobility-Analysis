1a668cc9598f6e56df7f3c81e63e4dcea6574380
jtwang7 <957113460@qq.com>
2022-02-04 12:21:30 +0800

    drag

---
+++ WebVisualizationSystem/src/common/hooks/useDrag.js
@@ -0,0 +1,54 @@
+// 拖拽移动
+import {useEffect} from 'react';
+
+export const useDrag = function (dom) {
+  useEffect(() => {
+    if (!dom) return () => {};
+
+    let isDraggable = false; // 是否可拖拽
+    let mousePos = {x: 0, y: 0}; // 鼠标位置
+    let targetPos = {x: 0, y: 0}; // 目标位置
+  
+    const onMouseDown = (e) => {
+      mousePos = {
+        x: e.clientX,
+        y: e.clientY,
+      };
+      targetPos = {
+        x: dom.offsetLeft,
+        y: dom.offsetTop,
+      }
+      if (!isDraggable) {
+        isDraggable = true;
+      }
+    }
+    const onMouseMove = (e) => {
+      if (!isDraggable) return;
+      const curMousePos = {
+        x: e.clientX,
+        y: e.clientY,
+      };
+      const deltaMousePos = {
+        deltaX: (curMousePos.x - mousePos.x),
+        deltaY: (curMousePos.y - mousePos.y),
+      };
+      console.log(targetPos.x, targetPos.x + deltaMousePos.deltaX);
+      dom.style.setProperty('left', `${targetPos.x + deltaMousePos.deltaX}px`);
+      dom.style.setProperty('top', `${targetPos.y + deltaMousePos.deltaY}px`);
+    }
+    const onMouseUp = (e) => {
+      if (isDraggable) {
+        isDraggable = false;
+      }
+    }
+    
+    dom.addEventListener('mousedown', onMouseDown, false);
+    dom.addEventListener('mousemove', onMouseMove, false);
+    dom.addEventListener('mouseup', onMouseUp, false);
+    return () => {
+      dom.removeEventListener('mousedown', onMouseDown);
+      dom.removeEventListener('mousemove', onMouseMove);
+      dom.removeEventListener('mouseup', onMouseUp);
+    }
+  }, [dom])
+}
--- WebVisualizationSystem/src/project/predict/components/modal/Modal.js
+++ WebVisualizationSystem/src/project/predict/components/modal/Modal.js
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React, { useRef } from 'react';
 import './Modal.scss';
 import {
   CloseOutlined,
   MinusOutlined,
   FullscreenOutlined,
 } from '@ant-design/icons';
+import { useDrag } from '@/common/hooks/useDrag';
 
 export default function Modal(props) {
   const {
@@ -12,18 +13,23 @@ export default function Modal(props) {
     setVisible,
   } = props;
 
+  // 样式
   const style = {
     width: '800px',
     height: '500px',
   }
+  const isDraggable = true;  // 是否可拖拽
 
-  const onClose = () => { setVisible(false) }
+  const onClose = () => { setVisible(false) }  // 销毁组件
+
+  const ref = useRef();
+  useDrag(ref.current);
 
   return (
     <>
       {
         isVisible ? (
-          <div style={style} className='modal-ctn' >
+          <div style={style} className='modal-ctn' ref={ref} >
             <header className='modal-header-bar'>
               <button className='macos-btn' style={{ backgroundColor: '#ffbf2b' }} onClick={onClose}>
                 <CloseOutlined className='macos-btn-icon' />

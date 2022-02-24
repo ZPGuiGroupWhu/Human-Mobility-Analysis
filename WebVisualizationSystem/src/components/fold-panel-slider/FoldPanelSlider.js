bbe24d4d07d2c9def31b21ab345efc1b5500cc28
jtwang7 <957113460@qq.com>
2022-02-24 11:24:16 +0800

    update fold-panel-slider

--- WebVisualizationSystem/src/components/fold-panel-slider/FoldPanelSlider.js
+++ WebVisualizationSystem/src/components/fold-panel-slider/FoldPanelSlider.js
@@ -1,10 +1,10 @@
-import React, { useEffect, useMemo, useRef, useState } from 'react';
+import React, { useMemo, useState } from 'react';
 import './FoldPanelSlider.scss';
 import _ from 'lodash';
-import { use } from 'echarts';
+import { CaretUpOutlined, CaretDownOutlined } from '@ant-design/icons';
 
 export default function FoldPanelSlider(props) {
-  const { style, mainComponents, minorComponents, setBottomDrawerHeight } = props;
+  const { style, mainComponents, minorComponents } = props;
   const footerHeight = '15px';
 
   const mains = useMemo(() => (Array.isArray(mainComponents) ? mainComponents : [mainComponents]), [mainComponents]);
@@ -15,23 +15,63 @@ export default function FoldPanelSlider(props) {
     setFold(prev => (!prev));
   }
 
-  const handleMouseLeave = useMemo(() => { return _.debounce(() => { setFold(true) }, 300) }, []);
+  const [isIconShow, setIconShow] = useState(false); // 是否显示icon提示
+
+  const handleMouseLeave = useMemo(() => { return _.debounce(() => { setFold(true); setIconShow(false) }, 300) }, []);
+  const handleMouseEnter = () => { 
+    setIconShow(true); 
+    handleMouseLeave.cancel(); // 取消防抖注册的延迟回调 
+  };
 
 
   return (
-    <div className='fold-panel-slider-ctn' style={{ ...style }} onMouseLeave={handleMouseLeave}>
-      <div style={{margin: footerHeight}}>
+    <div
+      className='fold-panel-slider-ctn'
+      style={{ ...style }}
+      onMouseLeave={handleMouseLeave}
+      onMouseEnter={handleMouseEnter}
+    >
+      <div style={{ margin: footerHeight }}>
         <section className='fold-panel-slider-content'>
           {mains}
         </section>
         <section
           className='fold-panel-slider-content minor'
-          style={{ overflow: 'hidden', maxHeight: isFold ? '0px' : '400px' }}
+          style={{ maxHeight: isFold ? '0px' : '400px' }}
         >
           {minors}
         </section>
       </div>
-      <footer style={{height: footerHeight}} className='fold-panel-slider-footer' onClick={handleClick}></footer>
+
+      <footer style={{ height: footerHeight }} className='fold-panel-slider-footer' onClick={handleClick}>
+        {
+          isFold ? (
+            <CaretUpOutlined
+              className='icon-animation'
+              style={{
+                color: '#fff',
+                position: 'absolute',
+                bottom: !isIconShow ? '-30px' : '-4px',
+                left: '50%',
+                transform: 'translateX(-50%)',
+                fontSize: '30px'
+              }}
+            />
+          ) : (
+            <CaretDownOutlined
+              className='icon-animation'
+              style={{
+                color: '#fff',
+                position: 'absolute',
+                bottom: !isIconShow ? '-30px' : '-4px',
+                left: '50%',
+                transform: 'translateX(-50%)',
+                fontSize: '30px'
+              }}
+            />
+          )
+        }
+      </footer>
     </div>
   )
 }

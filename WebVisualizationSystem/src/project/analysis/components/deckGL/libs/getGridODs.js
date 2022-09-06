925417c59a56d0b058b9eeb79941c10b41d871e4
Douc1998 <cugdc@cug.edu.cn>
2022-09-06 11:43:20 +0800

    poiSelect

--- WebVisualizationSystem/src/project/analysis/components/deckGL/libs/getGridODs.js
+++ WebVisualizationSystem/src/project/analysis/components/deckGL/libs/getGridODs.js
@@ -5,9 +5,8 @@ import _ from 'lodash';
 
 
 // 统计 O / D 落入 poi块 的轨迹编号
-export const getGridODs = (userData, cellCenter, dis) => {
+export const getGridODsSingle = (userData, cellCenter, dis) => {
     const [centerLng, centerLat] = cellCenter;
-    console.log(centerLng, centerLat)
     const selectedIDs = userData.reduce((prev, cur) => {
         const isPush = getFlatternDistance(centerLat, centerLng, cur?.origin?.[1], cur?.origin?.[0]) <= dis ||
             getFlatternDistance(centerLat, centerLng, cur?.destination?.[1], cur?.destination?.[0]) <= dis;
@@ -17,4 +16,27 @@ export const getGridODs = (userData, cellCenter, dis) => {
         return prev;
     }, [])
     return selectedIDs;
+}
+
+// 统计 O 和 D 都落入两个 poi块 的轨迹编号
+export const getGridODsDouble = (userData, cellCenter1, cellCenter2, dis) => {
+    const [centerLng1, centerLat1] = cellCenter1;
+    const [centerLng2, centerLat2] = cellCenter2;
+    const selectIDs = userData.reduce((prev, cur) => {
+        const isPush =
+            (
+                getFlatternDistance(centerLat1, centerLng1, cur?.origin?.[1], cur?.origin?.[0]) <= dis &&
+                getFlatternDistance(centerLat2, centerLng2, cur?.destination?.[1], cur?.destination?.[0]) <= dis
+            )
+            ||
+            (
+                getFlatternDistance(centerLat1, centerLng1, cur?.destination?.[1], cur?.destination?.[0]) <= dis &&
+                getFlatternDistance(centerLat2, centerLng2, cur?.origin?.[1], cur?.origin?.[0]) <= dis
+            )
+        if(isPush){
+            prev.push(cur.id);
+        }
+        return prev;
+    }, [])
+    return selectIDs;
 }
